fbd7069500b0075d753d34408213ef943ec45681
LayaFlo <42862790+LayaFlo@users.noreply.github.com>
2019-09-12 09:00:17 +0300

    Add files via upload

---
+++ ex8todolistDeleteButton/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const deleteTodo = (event) => {
+    event.preventDefault();
+    console.log(event.target.id);
+    setTodos(todos.filter((todo, index) => parseInt(event.target.id) !== index));
+  }
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+  
+  return (
+    <div className="App">
+      <header className="App-header">Simple TodoList</header> 
+      <form onSubmit={addTodo}>
+        Description: <input type="text" name="desc" onChange={inputChanged} value={todo.desc}/>
+        Date: <input type="date" name="date" onChange={inputChanged} value={todo.date}/>
+        <input type="submit" value="Add"/>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+        {todos.map((todo, index) => <tr key={index}><td>{todo.date}</td><td>{todo.desc}</td><td><button id={index} onClick={deleteTodo}>Delete</button></td></tr>)}  
+      </tbody></table>
+    </div>
+        
+  );
+}
+
+export default App;
