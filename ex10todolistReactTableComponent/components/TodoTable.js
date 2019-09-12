5e7cc599474d525237c2b37e31f1d16a8eae0f84
LayaFlo <42862790+LayaFlo@users.noreply.github.com>
2019-09-12 10:42:16 +0300

    Add files via upload

---
+++ ex10todolistReactTableComponent/components/TodoTable.js
@@ -0,0 +1,25 @@
+import React from 'react'; 
+import ReactTable from 'react-table'; 
+import 'react-table/react-table.css';
+
+const TodoTable = (props) => { 
+    const columns = [{ 
+        Header: 'Date', 
+        accessor: 'date' // String-based value accessors! 
+    }, { 
+        Header: 'Description', 
+        accessor: 'desc', 
+    }, {
+        Cell: ({index}) => (
+            <button id={index} onClick={props.deleteTodo}>Delete</button>
+          )
+    }]
+    return ( 
+        <div className="App"> 
+            <ReactTable data={props.todos}    
+            columns={columns} sortable={true} 
+            defaultPageSize={10} /> 
+        </div> 
+    );
+} 
+export default TodoTable;
